46f87a57aa31601e74fa8ec655aa6dff431c26c9
Diwadkar <diwadkar@seas.upenn.edu>
2019-04-24 16:39:20 -0400

    Data visualization features for any uploaded data file.

--- data_visualization/ui.R
+++ data_visualization/ui.R
@@ -25,7 +25,7 @@ shinyUI(fluidPage(
              br(),
              dataTableOutput("contents")),
              br(),hr(),
-             h4(p("Example Phenotype Dataset")),
+             h4(p("Example Dataset:")),
              p("Here, as an example, we have the phenotype information for the Gene Expression Omnibus (GEO) dataset : ", 
                a(href="http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE8823", "GSE8823.", target="_blank"),"This dataset was for an experiment in which
                investigators compared gene expression profiles of bronchoalveolar lavage fluid obtained from 11 non-smokers
@@ -35,7 +35,7 @@ shinyUI(fluidPage(
              br(),
              dataTableOutput("phenoData")),
             
-    tabPanel("Data Visualization",
+    tabPanel("Example Dataset",
               br(),
              h3(p("Univariate Analysis")),
              h4(p("Categorical Variables")),
@@ -43,6 +43,10 @@ shinyUI(fluidPage(
              selectInput("feat",label="Select variable:",choices=c("Treatment","Sex","Ancestry","ScanDate_Group"),multiple=FALSE,width="220px",selected = "Treatment"),
              plotOutput("barPlot",height="500px",width="800px"),
              br(),hr(),
+             p("We can explore the phenotype data attributes with respect to the two main groups - smoker versus non-smoker."),
+             selectInput("var",label="Select feature:",choices=c("Sex","Ancestry","ScanDate_Group"),multiple=FALSE,width="220px",selected = "Sex"),
+             plotOutput("fbarPlot",height="500px",width="1100px"),
+             hr(),
              h4(p("Continuous Variables")),
              p("A histogram shows the distribution of any numerical data using a single variable as input.The variable is cut into multiple bins, where the height of the bin represents the number of observations per bin. Here, we are using bins of size=2."),
              plotOutput("histPlot",height="500px",width="800px"),
@@ -50,5 +54,33 @@ shinyUI(fluidPage(
              h3(p("Bivariate Analysis")),
              h4(p("Continuous Variable vs. Categorical Variable")),
              p("The boxplot gives summary of numerical values. The line in the middle denotes the median while the upper and lower lines denote upper and lower quartiles."),
-             plotOutput("boxPlot",height="500px",width="800px")))
+             selectInput("comp",label="Select feature:",choices=c("Sex","Ancestry","Treatment"),multiple=FALSE,width="220px",selected = "Treatment"),
+             plotOutput("boxPlot",height="500px",width="800px")),
+    
+    tabPanel("Data Visualization",
+            br(),
+            h3(p("Overview of data")),
+            h4(p("The continuous variables in the uploaded dataset are as follows:")),
+            conditionalPanel(condition = "!is.null(output.contents)", dataTableOutput("varcData"),
+            br(),
+            h4(p("The categorical variables in the uploaded dataset are as follows:")),
+            dataTableOutput("vardData"),
+            br(), hr(),
+            h3(p("Univariate Analysis")),
+            h4(p("Categorical Variables")),
+            p("The distribution for the categorical variables with number of levels between 2-10 is displayed here."),
+            uiOutput('disc'),
+            plotOutput("barplotUP",height="500px",width="800px"),
+            br(), hr(),
+            h4(p("Continuous Variables")),
+            uiOutput('cont'),
+            plotOutput("fbarplotUP",height="500px",width="800px"),
+            br(),
+            p("Histogram showing distribution of continuous variables"),
+            plotOutput("histPlotUP",height="500px",width="800px"),
+            br(),
+            p("Boxplot showing summary of variables"),
+            plotOutput("boxPlotUP",height="500px",width="800px"))
+            ))
+  
 ))
