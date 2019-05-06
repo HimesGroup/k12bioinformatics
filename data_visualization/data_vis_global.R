6aa611e7c8f46f510d6e3bf1199720a1570f191a
Diwadkar <diwadkar@seas.upenn.edu>
2019-05-06 13:33:06 -0400

    Set colors for figures, better scaling, helper functions in new global script.

---
+++ data_visualization/data_vis_global.R
@@ -0,0 +1,94 @@
+
+#########################
+## Read in data files ##
+########################
+pheno_QC <- read.table("../gene_expression/databases/GSE8823_Phenotype_withQC.txt", sep="\t", header=TRUE)
+pheno_QC <- pheno_QC %>% dplyr::filter(QC_Pass!=0) %>% rename(ScanDate=ScanDate_Group)
+pheno_QC$ScanDate <- as.factor(gsub("/([^/]*)$", " ", pheno_QC$ScanDate))
+pheno_QC$Treatment <- as.factor(gsub("_","-",pheno_QC$Treatment))
+pheno_QC$Donor <- as.factor(paste0("D",pheno_QC$Donor))
+
+
+###############
+## FUNCTIONS ##
+###############
+
+##BARPLOT##
+barplot_func <- function(x,data){
+  color_status <- set_colors(data)
+  g1 <- ggplot(data, aes_string(x=x)) + geom_bar(stat="count",aes_string(fill=x)) + scale_fill_manual(values=unlist(lapply(levels(data[[x]]), function(y) color_status[[y]]))) + theme_bw() +
+    theme(legend.text = element_text(size=14),
+          axis.title=element_text(size=15),
+          title = element_text(size=15),
+          axis.text=element_text(size=14))
+  return(g1) 
+  
+}
+
+#sample(colours, length(levels(data[[x]])))
+barplot_both_func <- function(x,y,data){
+  color_status <- set_colors(data)
+  g1 <- ggplot(data, aes_string(x=x,y=y)) + geom_bar(stat="identity",aes_string(fill=x)) +  
+    scale_fill_manual(values=unlist(lapply(levels(data[[x]]), function(m) color_status[[m]]))) + theme_bw() +
+    theme(legend.text = element_text(size=14),
+          axis.title=element_text(size=15),
+          title = element_text(size=15),
+          axis.text=element_text(size=14))
+  return(g1) 
+  
+}
+
+##BOXPLOT##
+boxplot_func <- function(x,y,data){
+  color_status <- set_colors(data)
+  ggplot(data, aes_string(x=x,y=y,fill=x)) + geom_boxplot(outlier.colour=NA, lwd=0.2, color="grey18") + 
+    stat_boxplot(geom ='errorbar', color="grey18") + 
+    labs(x=x, y=y) + geom_jitter() + 
+    scale_fill_manual(values=unlist(lapply(levels(data[[x]]), function(m) color_status[[m]]))) + theme_bw() +
+    theme(legend.text = element_text(size=14),
+          axis.title=element_text(size=15),
+          title = element_text(size=15),
+          axis.text=element_text(size=14))}
+
+##HISTOGRAM##
+hist_func <- function(Con,data){
+  df <- data[[Con]]
+  ggplot(data=data, aes_string(Con)) + geom_histogram(breaks=seq(min(df), max(df)),col="#D55E00", fill="#56B4E9",alpha=0.6) + labs(x=Con, y="Count") + xlim(c(min(df),max(df))) + 
+    theme_bw() +
+    theme(legend.text = element_text(size=14),
+          axis.title=element_text(size=15),
+          title = element_text(size=15),
+          axis.text=element_text(size=14)) ##0072B2 #xlim: 35,50
+}
+
+########################
+## HELPER FUNCTIONS  ##
+########################
+
+get_discrete_var <- function(data){
+  disc_var_lst <- list()
+  all_vars <- names(data)[sapply(data, class) == "factor"]
+  for (i in seq(1,length(all_vars))){
+    len = length(levels(data[[all_vars[i]]]))
+    if (len >=2 && len <=10){
+      disc_var_lst[i] = all_vars[i]}
+  }
+  return(unlist(disc_var_lst))
+}
+
+#set colors
+color_status <- colours <- c("#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7","#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666","#8DD3C7","#BEBADA") #CB and Dark2
+
+set_colors <- function(data){
+  choices = get_discrete_var(data)
+  count = 0 
+  color_list = list()
+  for (i in choices) { 
+    color_list =  append(color_list,levels(data[[i]]))
+  }
+  names(color_status) <- color_list
+  
+  return(color_status)
+}
+
+
--- data_visualization/server.R
+++ data_visualization/server.R
@@ -3,13 +3,8 @@ library(plyr)
 library(dplyr)
 library(ggplot2)
 #library(rCharts) #install with devtools (install_github("rCharts", "ramnathv"))
+source("data_vis_global.R")
 
-pheno_QC <- read.table("../gene_expression/databases/GSE8823_Phenotype_withQC.txt", sep="\t", header=TRUE)
-pheno_QC <- pheno_QC %>% dplyr::filter(QC_Pass!=0)
-pheno_QC$Treatment <- as.factor(gsub("_","-",pheno_QC$Treatment))
-pheno_QC$Donor <- as.factor(paste0("D",pheno_QC$Donor))
-#pheno_QC <- pheno_QC %>% mutate_all(as.factor)
-colours = c("#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7","#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666") #CB and Dark2
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output){
@@ -17,56 +12,17 @@ shinyServer(function(input, output){
 
   output$phenoData <- renderDataTable({pheno_QC %>% dplyr::select(GEO_ID, Smoking_status, Sex, Age, Ancestry,Donor)},options = list(pageLength=10, searching=FALSE))
   
-  ##BARPLOT##
-  #function
-  barplot_func <- function(x,data){
-    g1 <- ggplot(data, aes_string(x=x)) + geom_bar(stat="count",aes_string(fill=x)) + scale_fill_manual(values=sample(colours, length(levels(data[[x]])))) + theme_bw() +
-      theme(legend.text = element_text(size=14),
-            axis.title=element_text(size=15),
-            title = element_text(size=15),
-            axis.text=element_text(size=14))
-    return(g1) 
-    
-  }
-  
-  barplot_both_func <- function(x,y,data){
-    g1 <- ggplot(data, aes_string(x=x,y=y)) + geom_bar(stat="identity",aes_string(fill=x)) + scale_fill_manual(values=sample(colours, length(levels(data[[x]])))) + theme_bw() +
-      theme(legend.text = element_text(size=14),
-            axis.title=element_text(size=15),
-            title = element_text(size=15),
-            axis.text=element_text(size=14))
-    return(g1) 
-    
-  }
   
   #output
   output$barPlot <- renderPlot({barplot_func(input$feat,pheno_QC)})
   
   output$fbarPlot <- renderPlot({barplot_func(input$var,pheno_QC) + facet_grid(.~Treatment) + theme(strip.text = element_text(size=15))})
   
-  ##HISTOGRAM##
-  #function
-  hist_func <- function(Con,data){
-    df <- data[[Con]]
-    ggplot(data=data, aes_string(Con)) + geom_histogram(breaks=seq(min(df), max(df)),col="#D55E00", fill="#56B4E9",alpha=0.6) + labs(x=Con, y="Count") + xlim(c(min(df),max(df))) + 
-    theme_bw() +
-      theme(legend.text = element_text(size=14),
-            axis.title=element_text(size=15),
-            title = element_text(size=15),
-            axis.text=element_text(size=14)) ##0072B2 #xlim: 35,50
-  }
+  
   #output
   output$histPlot <- renderPlot({hist_func("Age",pheno_QC)})
   
-  ##BOXPLOT##
-  #function
-  boxplot_func <- function(var,age,data){ggplot(data, aes_string(x=var,y=age,fill=var)) + geom_boxplot(outlier.colour=NA, lwd=0.2, color="grey18") + 
-      stat_boxplot(geom ='errorbar', color="grey18") + 
-      labs(x=var, y=age) + geom_jitter() + theme_bw() +
-      theme(legend.text = element_text(size=14),
-            axis.title=element_text(size=15),
-            title = element_text(size=15),
-            axis.text=element_text(size=14))}
+  
   #output
   output$boxPlot <- renderPlot({boxplot_func(input$comp,"Age",pheno_QC)})
   
@@ -78,14 +34,7 @@ shinyServer(function(input, output){
   
   disc_var <- reactive({
     if(!is.null(contents())){
-      disc_var_lst <- list()
-      all_vars <- names(contents())[sapply(contents(), class) == "factor"]
-      for (i in seq(1,length(all_vars))){
-        len = length(levels(contents()[[all_vars[i]]]))
-        if (len >=2 && len <=10){
-          disc_var_lst[i] = all_vars[i]}
-      }
-      unlist(disc_var_lst)
+      get_discrete_var(contents())
     } else { NULL }
 })
     
