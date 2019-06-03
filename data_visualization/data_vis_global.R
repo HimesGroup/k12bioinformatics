a507c418561160b889503554f0c695c4b56d0ef6
Diwadkar <diwadkar@seas.upenn.edu>
2019-06-03 16:03:44 -0400

    Removed example dataset, debugged warning signs, removed univariate barplot, rescaled histogram and other edits.

--- data_visualization/data_vis_global.R
+++ data_visualization/data_vis_global.R
@@ -1,13 +1,40 @@
 
-#########################
-## Read in data files ##
 ########################
-pheno_QC <- read.table("../databases/GSE8823_Phenotype_withQC.txt", sep="\t", header=TRUE)
-pheno_QC <- pheno_QC %>% dplyr::filter(QC_Pass!=0) %>% rename(ScanDate=ScanDate_Group)
-pheno_QC$ScanDate <- as.factor(gsub("/([^/]*)$", " ", pheno_QC$ScanDate))
-pheno_QC$Treatment <- as.factor(gsub("_","-",pheno_QC$Treatment))
-pheno_QC$Donor <- as.factor(paste0("D",pheno_QC$Donor))
-iris_data <- read.csv("../databases/iris_dataset.csv")
+## HELPER FUNCTIONS  ##
+########################
+
+get_discrete_var <- function(data){
+  disc_var_lst <- list()
+  vars1 <- names(data)[sapply(data, class) == "factor"]
+  toMatch <- c("NAME","CODE","LATITUDE","LONGITUDE")
+  vars2 <- names(data)[grep(paste(toMatch,collapse="|"),names(data),ignore.case = TRUE)]
+  vars3 <- names(data)[grep("ID",names(data))]
+  all_vars <- unique(append(vars1,append(vars2,vars3)))
+  
+  for (i in seq(1,length(all_vars))){
+    len = length(unique(data[[all_vars[i]]]))
+    if (len >=2 && len <=15){
+      #if (len <=15){
+      disc_var_lst[i] = all_vars[i]}
+  }
+  return(unlist(disc_var_lst))
+}
+
+get_cont_var <- function(data){
+  cont_var_lst <- list()
+  vars1 <- names(data)[sapply(data, class) != "factor"]
+  toMatch <- c("NAME","CODE","LATITUDE","LONGITUDE")
+  vars2 <- names(data)[grep(paste(toMatch,collapse="|"),names(data),ignore.case = TRUE)]
+  vars3 <- names(data)[grep("ID",names(data))]
+  all_vars <- setdiff(vars1,append(vars2,vars3))
+  
+  for (i in seq(1,length(all_vars))){
+    len = length(unique(data[[all_vars[i]]]))
+    if (len >=2 && len <=15){
+      cont_var_lst[i] = all_vars[i]}
+  }
+  return(unlist(cont_var_lst))
+}
 
 
 ###############
@@ -21,8 +48,15 @@ colours <- c("#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7",
                              brewer.pal(11, "Set3"),
                              brewer.pal(11, "Paired")) 
 
+get_data <- function(data){
+  choices = get_discrete_var(data)
+  data[choices] <- lapply(data[choices], factor) 
+  return(data)
+}
+
 set_colors <- function(data){
   choices = get_discrete_var(data)
+  data <- get_data(data)
   color_list = list()
   for (i in choices) { 
     color_list =  append(color_list,levels(data[[i]]))
@@ -35,6 +69,7 @@ set_colors <- function(data){
 
 ##BARPLOT##
 barplot_func <- function(x,data){
+  data <- get_data(data)
   color_status <- set_colors(data)
   g1 <- ggplot(data, aes_string(x=x,fill=x)) + geom_bar(stat="count") + 
     scale_fill_manual(values=unlist(lapply(levels(data[[x]]), function(y) color_status[[y]]))) + theme_bw() +
@@ -47,6 +82,7 @@ barplot_func <- function(x,data){
 }
 
 barplot_func_dodge <- function(x,a,data){
+  data <- get_data(data)
   color_status <- set_colors(data)
   g1 <- ggplot(data, aes_string(x=x,fill=a)) + geom_bar(stat="count",position=position_dodge(preserve = "single")) + 
     scale_fill_manual(values=unlist(lapply(levels(data[[a]]), function(y) color_status[[y]]))) + theme_bw() +
@@ -60,6 +96,7 @@ barplot_func_dodge <- function(x,a,data){
 
 #sample(colours, length(levels(data[[x]])))
 barplot_both_func <- function(x,y,data){
+  data <- get_data(data)
   color_status <- set_colors(data)
   g1 <- ggplot(data, aes_string(x=x,y=y,fill=x)) + geom_bar(stat="identity") +  
     scale_fill_manual(values=unlist(lapply(levels(data[[x]]), function(m) color_status[[m]]))) + theme_bw() +
@@ -73,6 +110,7 @@ barplot_both_func <- function(x,y,data){
 
 ##BOXPLOT##
 boxplot_func <- function(x,y,data){
+  data <- get_data(data)
   color_status <- set_colors(data)
   ggplot(data, aes_string(x=x,y=y,fill=x)) + geom_boxplot(outlier.colour=NA, lwd=0.2, color="grey18",na.rm=TRUE) + 
     stat_boxplot(geom ='errorbar', color="grey18") + 
@@ -85,31 +123,18 @@ boxplot_func <- function(x,y,data){
 
 ##HISTOGRAM##
 hist_func <- function(Con,data){
+  data <- get_data(data)
   df <- data[[Con]]
-  bw <- nclass.Sturges(df)
-  #bw <- (2 * IQR(df)) / length(df)^(1/3) #Freedman–Diaconis rule 
-  ggplot(data=data, aes_string(x=Con)) + geom_histogram(col="#D55E00", fill="#56B4E9",alpha=0.6,binwidth = bw,na.rm=TRUE) + labs(x=Con, y="Count") +  
+  bins = round(sqrt(length(df)))
+  #bw <- nclass.Sturges(df)
+  #bw <- (2 * IQR(df))/ length(df)^(1/3) #Freedman–Diaconis rule 
+  #breaks <- pretty(range(df), n = nclass.FD(df), min.n = 1)
+  #bwidth <- breaks[2]-breaks[1]
+  ggplot(data=data, aes_string(x=Con)) + geom_histogram(col="#D55E00", fill="#56B4E9",alpha=0.6,bins = bins ,na.rm=TRUE) + labs(x=Con, y="Count") +  
     theme_bw() +
     theme(legend.text = element_text(size=14),
           axis.title=element_text(size=15),
           title = element_text(size=15),
           axis.text=element_text(size=13)) ##0072B2 #xlim: 35,50
 }
-
-########################
-## HELPER FUNCTIONS  ##
-########################
-
-get_discrete_var <- function(data){
-  disc_var_lst <- list()
-  all_vars <- names(data)[sapply(data, class) == "factor"]
-  for (i in seq(1,length(all_vars))){
-    len = length(levels(data[[all_vars[i]]]))
-    if (len >=2 && len <=15){
-      disc_var_lst[i] = all_vars[i]}
-  }
-  return(unlist(disc_var_lst))
-}
-
-
 
--- data_visualization/ui.R
+++ data_visualization/ui.R
@@ -24,43 +24,6 @@ shinyUI(fluidPage(
              br(),
              dataTableOutput("contents")),
              br(),hr()),
-             
-            
-    tabPanel("Example Dataset",
-              br(),
-             p("Here, as an example, we have the phenotype information for the Gene Expression Omnibus (GEO) dataset : ", 
-               a(href="http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE8823", "GSE8823.", target="_blank"),"This dataset was for an experiment in which
-               investigators compared gene expression profiles of bronchoalveolar lavage fluid obtained from 11 non-smokers
-               versus 13 smokers (mean smoking use was 36 pack-years). The scientific paper published to report the results of
-               this experiment can be found",
-               a(href="https://www.ncbi.nlm.nih.gov/pubmed/18587056", "here.", target="_blank")),
-             dataTableOutput("phenoData"),
-             br(),br(),
-             h3(p("Univariate Analysis")),
-             h4(p("Categorical Variables")),
-             p("Here, we can explore the phenotype data attributes. First, lets look at the distribution of all the variables using barplots. 
-               A barplot (or barchart) is one of the most common type of plot. 
-               It shows the relationship between a numerical variable and a categorical variable."),
-             selectInput("feat",label="Select variable:",choices=c("Treatment","Sex","Ancestry","ScanDate"),multiple=FALSE,width="220px",selected = "Treatment"),
-             plotOutput("barPlot",height="400px",width="550px"),
-             br(),hr(),
-             p("We can explore the phenotype data attributes with respect to the two main groups - smoker versus non-smoker."),
-             selectInput("var",label="Select feature:",choices=c("Sex","Ancestry","ScanDate"),multiple=FALSE,width="220px",selected = "Sex"),
-             plotOutput("fbarPlot",height="400px",width="750px"),
-             hr(),
-             h4(p("Continuous Variables")),
-             p("A histogram shows the distribution of any numerical data using a single variable as input.
-               The variable is cut into multiple bins, where the height of the bin represents the number of observations per bin. 
-               Here, we are using bins of size=2."),
-             plotOutput("histPlot",height="400px",width="550px"),
-             br(),hr(),
-             h3(p("Bivariate Analysis")),
-             h4(p("Continuous Variable vs. Categorical Variable")),
-             p("The boxplot gives summary of numerical values. The line in the middle denotes the median while the upper 
-               and lower lines denote upper and lower quartiles."),
-             selectInput("comp",label="Select feature:",choices=c("Sex","Ancestry","Treatment"),multiple=FALSE,width="220px",selected = "Treatment"),
-             plotOutput("boxPlot",height="400px",width="550px")),
-    
     tabPanel("Univariate Analysis",
             br(),
             h3(p("Overview of data")),
@@ -71,21 +34,23 @@ shinyUI(fluidPage(
             dataTableOutput("vardData"),
             br(), hr(),
             h4(p("Categorical Variables")),
-            p("The distribution for the categorical variables with number of levels between 2-15 is displayed here."),
+            p("The distribution for the categorical variables with number of unique entries between 2-15 are displayed here."),
             uiOutput('disc'),
             plotOutput("barplotUP",height="400px",width="auto"),
             br(), hr(),
             h4(p("Continuous Variables")),
+            p("The distribution for the continuous variables with number of unique entries between 2-15 are displayed here."),
             uiOutput('cont'),
             br(),
             p("Histogram showing distribution of continuous variables"),
             plotOutput("histPlotUP",height="400px",width="auto"),
             br()),
+    
     tabPanel("Bivariate Analysis", br(),
             p("Relationship between the categorical and continuous variables in the uploaded data"),
             uiOutput('bdisc'),
             uiOutput('bcont'),
-            plotOutput("fbarplotUP",height="400px",width="auto"),
+            #plotOutput("fbarplotUP",height="400px",width="auto"),
             p("Boxplot showing summary of variables"),
             plotOutput("boxPlotUP",height="400px",width="auto")))
   
