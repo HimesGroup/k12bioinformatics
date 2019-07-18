af35970f9014c7fa3890ce18f2650386e3ecae9f
Blanca Himes <bhimes@pennmedicine.upenn.edu>
2019-07-18 14:36:19 -0400

    font change attempt

--- air_pollution/ui.R
+++ air_pollution/ui.R
@@ -57,7 +57,7 @@ shinyUI(fluidPage(
              hr(),
 
              h3(p("Sept 2017 mean PM2.5 measures")),
-             plotOutput("kbarPlot", height="400px", width="700px"), 
+             plotOutput("kbarPlot", width="700px"), 
              hr(),
     
              h3(p("Selected Cities")),
